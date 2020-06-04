14cd072961b4e674ac4a4df0ff78af9619ca82ab
Scott Baron <scottbaron50@outlook.com>
2020-06-04 12:20:30 -0400

    My first express app

--- blocking vs non-blocking.js
+++ blocking vs non-blocking.js
@@ -1,3 +1,28 @@
+console.clear();
 //  blocking [sync]
+const sayHi = name => console.log(`Hello, ${name}`);
+//This is the full function version of the above arrow function.
+/*
+function sayHi (name) {
+    console.log("Hello, " = name);
+}
+sayHi(`Shaun`);
+*/
+sayHi('Shaun');
+sayHi('Devon');
+sayHi('Keval');
 
 //  non-blocking [async]
+setTimeout(sayHi, 2500, 'Michael');
+setTimeout(sayHi, 3500, 'Gagandeep');
+setTimeout(sayHi, 1500, 'Bob');
+sayHi('Ilia');
+
+//  Callback
+const farewell = name => console.log(`Goodbye, ${name}`);
+const greeting = (name, callback) => {
+    console.log(`Hello, ${name}`);
+    callback(name);
+}
+
+greeting('Shaun McKinnon', sayHi);
